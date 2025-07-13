bb81f7f59fbc01a1ffccf8e79845201831ca7b6f
khobayer <khobayerkhan@gmail.com>
2025-07-13 14:40:49 +0600

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 //Dynamically import SimpleMDE with SSR disabled
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
@@ -31,6 +32,7 @@ const NewIssuePage = () => {
   });
 
   const [error, setError] = useState("");
+  const [isSubmiting, setSubmiting] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -43,9 +45,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmiting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmiting(false);
             setError("An unexpected error occurred.");
           }
         })}
@@ -60,7 +64,9 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmiting}>
+          Submit New Issue {isSubmiting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
