414b3c6f3005b610a2fea6db41f61171f0efefa4
khobayer <khobayerkhan@gmail.com>
2025-07-16 10:36:07 +0600

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -8,16 +8,25 @@ export async function PATCH(
     request: NextRequest, 
     { params }: { params: { id: string }}) {
         
-    const session = await getServerSession(authOptions);
+    // const session = await getServerSession(authOptions);
     
-    if (!session)
-        return NextResponse.json({}, { status: 401 });
+    // if (!session)
+    //     return NextResponse.json({}, { status: 401 });
 
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
 
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const { assignedToUserId, title, description } = body;
+
+    if (body.assignedToUserId) {
+        const user = await prisma.user.findUnique({ where: { id: assignedToUserId } });
+
+        if(!user)
+            return NextResponse.json({ error: "Invalid User." }, { status: 400 })
+    }
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
@@ -27,10 +36,11 @@ export async function PATCH(
         return NextResponse.json({ error: "Invalid issue"}, { status: 404 });
 
     const updatedIssue = await prisma.issue.update({
-        where: { id: issue.id},
+        where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description,
+            title,
+            description,
+            assignedToUserId
         }
     });
 
