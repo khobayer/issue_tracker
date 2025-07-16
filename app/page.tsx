8fad85a248f2183d1594127ed531734001833ec4
khobayer <khobayerkhan@gmail.com>
2025-07-17 02:15:00 +0600

    Fix pagination

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ interface Props {
   searchParams?: { page?: string };
 }
 
-export default function Home({ searchParams }: Props) {
-  const currentPage = Number(searchParams?.page) || 1;
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams; 
+  const currentPage = Number(params?.page) || 1;
 
   return <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />;
 }
